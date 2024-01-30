5354937f739bebc22ddd0e9160f76938043fb7e8
OSPT <onesinus231@gmail.com>
2024-01-30 15:55:12 +0700

    Initial commit

---
+++ estate/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': "Estate",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Onesinus Tamba",
+    'category': 'App',
+    'description': """
+        This module is used to learn basic odoo 17 technical
+    """,
+    'application': True,
+}
