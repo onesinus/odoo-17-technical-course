cdb372c9285f952e9cbb8fa83812ee30faa1813c
Onesinus Tamba <onesinus231@gmail.com>
2024-01-31 19:37:52 +0700

    Many2one relation

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -21,3 +21,6 @@ class EstateProperty(models.Model):
 	    string="Garden Orientation",
 	)
 	last_seen = fields.Datetime("Last Seen", default=fields.Datetime.now)
+
+	user_id = fields.Many2one("res.users", string="Salesman", default=lambda self: self.env.user)
+	buyer_id = fields.Many2one("res.partner", string="Buyer", readonly=True, copy=False)
