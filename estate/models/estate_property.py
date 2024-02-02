9eb051e00e3836b1b6780630f5980d7ce47c230c
Onesinus Tamba <onesinus231@gmail.com>
2024-02-02 07:50:51 +0700

    Actions button

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,5 +1,5 @@
 from odoo import models, fields, api
-from odoo.exceptions import ValidationError
+from odoo.exceptions import ValidationError, UserError
 
 class EstateProperty(models.Model):
 	_name = "estate.property"
@@ -54,3 +54,26 @@ class EstateProperty(models.Model):
 	user_id = fields.Many2one("res.users", string="Salesman", default=lambda self: self.env.user)
 	buyer_id = fields.Many2one("res.partner", string="Buyer", readonly=True, copy=False)
 	total_area = fields.Integer(readonly=True, compute="_compute_total_area")
+	state = fields.Selection(
+	    selection=[
+	        ("new", "New"),
+	        ("offer_received", "Offer Received"),
+	        ("offer_accepted", "Offer Accepted"),
+	        ("sold", "Sold"),
+	        ("canceled", "Canceled"),
+	    ],
+	    string="Status",
+	    required=True,
+	    copy=False,
+	    default="new",
+	)
+
+	def action_sold(self):
+	    if "canceled" in self.mapped("state"):
+	        raise UserError("Canceled properties cannot be sold.")
+	    return self.write({"state": "sold"})
+
+	def action_cancel(self):
+	    if "sold" in self.mapped("state"):
+	        raise UserError("Sold properties cannot be canceled.")
+	    return self.write({"state": "canceled"})
