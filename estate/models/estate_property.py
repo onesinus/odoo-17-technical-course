b1e2c8cc8e4f82a3152ddc5d4f4e27296a3a1872
Onesinus Tamba <onesinus231@gmail.com>
2024-02-02 07:33:52 +0700

    Compute and onchange fields

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -19,6 +19,20 @@ class EstateProperty(models.Model):
 	        if record.selling_price < 1:
 	            raise ValidationError("The selling price must be greater than 0")
 
+	@api.depends("living_area", "garden_area")
+	def _compute_total_area(self):
+	    for prop in self:
+	        prop.total_area = prop.living_area + prop.garden_area
+
+	@api.onchange("garden")
+	def _onchange_garden(self):
+	    if self.garden:
+	        self.garden_area = 10
+	        self.garden_orientation = "N"
+	    else:
+	        self.garden_area = 0
+	        self.garden_orientation = False
+
 	name = fields.Char(required=True)
 	description = fields.Text(default="Estate Property Description")
 	postcode = fields.Char()
@@ -39,3 +53,4 @@ class EstateProperty(models.Model):
 
 	user_id = fields.Many2one("res.users", string="Salesman", default=lambda self: self.env.user)
 	buyer_id = fields.Many2one("res.partner", string="Buyer", readonly=True, copy=False)
+	total_area = fields.Integer(readonly=True, compute="_compute_total_area")
