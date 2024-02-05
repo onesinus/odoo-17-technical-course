d27405077dfafb0678e88844fedf879420439024
Onesinus Tamba <onesinus231@gmail.com>
2024-02-05 09:38:56 +0700

    Consuming api from public api

--- estate/controllers/estate_property.py
+++ estate/controllers/estate_property.py
@@ -1,3 +1,6 @@
+import json
+import requests
+
 from odoo import http
 
 
@@ -9,3 +12,8 @@ class EstateProperty(http.Controller):
 	@http.route('/xyz', auth='user')
 	def xyz(self, **kw):
 		return "This is accessible for authenticated user"
+
+	@http.route('/entries', auth='public')
+	def entries(self, **kw):
+		response = requests.get("https://api.publicapis.org/entries")
+		return json.dumps(response.json())
