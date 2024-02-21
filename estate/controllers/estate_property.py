29a627c26931b319f082fba831ba9223f217bc35
Onesinus Tamba <onesinus231@gmail.com>
2024-02-21 13:43:52 +0700

    Add decorator for endpoint authentication

--- estate/controllers/estate_property.py
+++ estate/controllers/estate_property.py
@@ -2,6 +2,7 @@ import json
 import requests
 
 from odoo import http
+from ..utils.auth_utils import authenticate
 
 
 class EstateProperty(http.Controller):
@@ -14,6 +15,7 @@ class EstateProperty(http.Controller):
 		return "This is accessible for authenticated user"
 
 	@http.route('/entries', auth='public')
+	@authenticate
 	def entries(self, **kw):
 		response = requests.get("https://api.publicapis.org/entries")
 		return json.dumps(response.json())
---
+++ estate/utils/auth_utils.py
@@ -0,0 +1,26 @@
+from odoo.http import request, Response
+from odoo.exceptions import AccessError
+
+
+def authenticate(func):
+	def wrapper(*args, **kwargs):
+		try:
+			token = request.httprequest.headers.get('Authorization')
+			user = authenticate_user(token)
+
+			kwargs['user'] = user
+
+			return func(*args, **kwargs)
+		except AccessError as ae:
+			return Response(f"Un-authorized: {(str(ae))}", status=401)
+		except Exception as e:
+			return Response(f"Bad Request: {str(e)}", status=400)
+
+	return wrapper
+
+def authenticate_user(token):
+	user = request.env['res.users'].sudo().search([('auth_token', '=', token)])
+	if not user:
+		raise AccessError("Invalid authentication token")
+
+	return user
