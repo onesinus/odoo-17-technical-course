7f078e310e6c3baf33180b41f7a4ab76de5622fb
Onesinus Tamba <onesinus231@gmail.com>
2024-02-22 14:03:00 +0700

    Testing Update Data

--- estate/tests/test_estate_property_tag.py
+++ estate/tests/test_estate_property_tag.py
@@ -25,3 +25,12 @@ class TestEstatePropertyTag(TransactionCase):
 		tag = new_tag.read(['name', 'color'])
 		self.assertEqual(tag[0]['name'], 'TAG 002')
 		self.assertEqual(tag[0]['color'], 69)
+
+	def test_update_tag(self):
+		tag = self.estate_property_tag_model.create({
+			'name': 'TAG 003',
+			'color': 70
+		})
+
+		tag.write({'color': 6969})
+		self.assertEqual(tag.color, 6969)
